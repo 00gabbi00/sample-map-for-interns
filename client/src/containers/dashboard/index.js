4c05ebdeee68dae43aa226c7568ebc6aadd1b9fb
pamucchi <52271889+pamucchi@users.noreply.github.com>
2022-07-05 13:19:30 +0800

    additional files 2

--- client/src/containers/dashboard/index.js
+++ client/src/containers/dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { Button, Grid, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import { MapContext } from '../../context/MapContext'
 
